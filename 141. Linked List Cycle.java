41c02f3a51e8f097fca3f6b8c804168a2cf66052
Peter_Pei <mmiaomiao140@gmail.com>
2026-05-02 23:06:24 +0800

    Add files via upload

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,29 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (true) {
+            if (fast.next == null || fast.next.next == null) {
+                break;
+            }
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+            slow = slow.next;
+        }
+        return false;
+    }
+}
